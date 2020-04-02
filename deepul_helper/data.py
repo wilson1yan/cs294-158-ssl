6d5cac45c339bbb875a41e068f339e7fc7bd8623
Wilson Yan <wilson1.yan@berkeley.edu>
2020-04-01 17:21:41 -0700

    dset update

--- deepul_helper/data.py
+++ deepul_helper/data.py
@@ -4,50 +4,56 @@ from torchvision import datasets
 from torchvision import transforms
 
 
-def get_datasets(dataset):
-    if dataset == 'imagenet':
-        train_dir = osp.join('data', dataset, 'train')
-        val_dir = osp.join('data', dataset, 'val')
-        train_dataset = datasets.ImageFolder(
-            train_dir,
-            transforms.Compose([
+def get_transform(task, train=True):
+    transform = None
+    if task == 'context_encoder':
+        transform = transforms.Compose([
+            transforms.Scale(128),
+            transforms.CenterCrop(128),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    elif task == 'rotation':
+        pass
+    elif task == 'cpc':
+        if train:
+            transform = transforms.Compose([
                 transforms.RandomResizedCrop(256),
                 transforms.RandomHorizontalFlip(),
                 transforms.Grayscale(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,), (0.5,)),
             ])
-        )
-
-        val_dataset = datasets.ImageFolder(
-            val_dir,
-            transforms.Compose([
+        else:
+            transforms = transforms.Compose([
                 transforms.Resize(300),
                 transforms.CenterCrop(256),
                 transforms.Grayscale(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,), (0.5,)),
             ])
+    elif task == 'simclr':
+        pass
+    else:
+        raise Exception('Invalid task:', task)
+    
+    return transform
+
+
+def get_datasets(dataset, task):
+    if dataset == 'imagenet':
+        train_dir = osp.join('data', dataset, 'train')
+        val_dir = osp.join('data', dataset, 'val')
+        train_dataset = datasets.ImageFolder(
+            train_dir,
+            get_transform(task, train=True)
+        )
+
+        val_dataset = datasets.ImageFolder(
+            val_dir,
+            get_transform(task, train=False)
         )
 
         return train_dataset, val_dataset, len(train_dataset.classes)
-    elif dataset == 'cifar10':
-        root = osp.join('data', dataset)
-        train_dataset = datasets.CIFAR10(root, train=True, download=True,
-                                         transform=transforms.Compose([
-                                             transforms.Resize(300),
-                                             transforms.RandomHorizontalFlip(),
-                                             transforms.Grayscale(),
-                                             transforms.ToTensor(),
-                                             transforms.Normalize((0.5,), (0.5,))
-                                         ]))
-        test_dataset = datasets.CIFAR10(root, train=False, download=True,
-                                        transform=transforms.Compose([
-                                            transforms.Resize(300),
-                                            transforms.Grayscale(),
-                                            transforms.ToTensor(),
-                                            transforms.Normalize((0.5,), (0.5,))
-                                        ]))
-        return train_dataset, test_dataset, len(train_dataset.classes)
     else:
         raise Exception('Invalid dataset:', dataset)

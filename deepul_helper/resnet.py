7f057bb21659a633577f7b38bafdc723c5e55b50
Wilson Yan <wilson1.yan@berkeley.edu>
2020-04-16 14:55:39 -0700

    small fixes

--- deepul_helper/resnet.py
+++ deepul_helper/resnet.py
@@ -12,7 +12,7 @@ class BatchNormReLU(nn.Module):
 
         self.relu = relu
         self.bn = bn_cls(in_features, center=center, scale=scale)
-    
+
     def forward(self, x):
         x = self.bn(x)
         if self.relu:
@@ -29,7 +29,7 @@ def fixed_padding(inputs, kernel_size):
 
 
 class Conv2dFixedPad(nn.Module):
-    
+
     def __init__(self, in_channels, out_channels, kernel_size, stride):
         super().__init__()
 
@@ -38,13 +38,13 @@ class Conv2dFixedPad(nn.Module):
 
         self.stride = stride
         self.kernel_size = kernel_size
-    
+
     def forward(self, x):
         if self.stride > 1:
             x = fixed_padding(x, self.kernel_size)
         return self.conv(x)
 
-    
+
 class ResidualBlock(nn.Module):
 
     def __init__(self, in_channels, filters, stride, use_projection=False):
@@ -52,7 +52,7 @@ class ResidualBlock(nn.Module):
         if use_projection:
             self.proj_conv = Conv2dFixedPad(in_channels, filters, kernel_size=1, stride=stride)
             self.proj_bnr = BatchNormReLU(filters, relu=False)
-        
+
         self.conv1 = Conv2dFixedPad(in_channels, filters, kernel_size=3, stride=stride)
         self.bnr1 = BatchNormReLU(filters)
 
@@ -60,7 +60,7 @@ class ResidualBlock(nn.Module):
         self.bnr2 = BatchNormReLU(filters)
 
         self.use_projection = use_projection
-    
+
     def forward(self, x):
         shortcut = x
         if self.use_projection:
@@ -69,7 +69,7 @@ class ResidualBlock(nn.Module):
         x = self.bnr2(self.conv2(x))
 
         return F.relu(x + shortcut, inplace=True)
-    
+
 
 class BottleneckBlock(nn.Module):
 
@@ -80,7 +80,7 @@ class BottleneckBlock(nn.Module):
             filters_out = 4 * filters
             self.proj_conv = Conv2dFixedPad(in_channels, filters_out, kernel_size=1, stride=stride)
             self.proj_bnr = BatchNormReLU(filters_out, relu=False)
-        
+
         self.conv1 = Conv2dFixedPad(in_channels, filters, kernel_size=1, stride=1)
         self.bnr1 = BatchNormReLU(filters)
 
@@ -91,7 +91,7 @@ class BottleneckBlock(nn.Module):
         self.bnr3 = BatchNormReLU(4 * filters)
 
         self.use_projection = use_projection
-    
+
     def forward(self, x):
         shortcut = x
         if self.use_projection:
@@ -99,9 +99,9 @@ class BottleneckBlock(nn.Module):
         x = self.bnr1(self.conv1(x))
         x = self.bnr2(self.conv2(x))
         x = self.bnr3(self.conv3(x))
-        
+
         return F.relu(x + shortcut, inplace=True)
-    
+
 
 class BlockGroup(nn.Module):
 
@@ -109,13 +109,16 @@ class BlockGroup(nn.Module):
         super().__init__()
 
         self.start_block = block_fn(in_channels, filters, stride, use_projection=True)
+        if block_fn == BottleneckBlock:
+            in_channels = filters * 4
+        else:
+            in_channels = filters
+
         self.blocks = []
         for _ in range(1, blocks):
-            if block_fn == BottleneckBlock:
-                in_channels *= 4
             self.blocks.append(block_fn(in_channels, filters, 1))
         self.blocks = nn.Sequential(*self.blocks)
-    
+
     def forward(self, x):
         x = self.start_block(x)
         x = self.blocks(x)
@@ -138,8 +141,8 @@ class ResNet(nn.Module):
                 BatchNormReLU(64 * width_multiplier),
                 nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
             )
-        
-        self.group1 = BlockGroup(64 * width_multiplier, 64 * width_multiplier, 
+
+        self.group1 = BlockGroup(64 * width_multiplier, 64 * width_multiplier,
                                  block_fn=block_fn, blocks=layers[0], stride=1)
         self.group2 = BlockGroup(64 * width_multiplier, 128 * width_multiplier,
                                  block_fn=block_fn, blocks=layers[1], stride=2)
@@ -147,7 +150,7 @@ class ResNet(nn.Module):
                                  block_fn=block_fn, blocks=layers[2], stride=2)
         self.group4 = BlockGroup(256 * width_multiplier, 512 * width_multiplier,
                                  block_fn=block_fn, blocks=layers[3], stride=2)
-        
+
     def forward(self, x):
         x = self.stem(x)
         x = self.group1(x)
@@ -169,6 +172,6 @@ def resnet_v1(resnet_depth, width_multiplier, cifar_stem=False):
 
     if resnet_depth not in model_params:
         raise ValueERror('Not a valid resnet_depth:', resnet_depth)
-    
+
     params = model_params[resnet_depth]
     return ResNet(params['block'], params['layers'], width_multiplier, cifar_stem=cifar_stem)

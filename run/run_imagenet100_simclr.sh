fb22995e537fe645d3aa7f68f8cf16ea0b61c966
Wilson Yan <wilson1.yan@berkeley.edu>
2020-04-19 13:44:52 -0700

    updates

--- run/run_imagenet100_simclr.sh
+++ run/run_imagenet100_simclr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Trained on 4 Titan Xps. Turn down batch size to use less GPU memory. If your batch size is <= 256, then set -u 0 (no warmup)
-python train_self_supervised_task.py -d imagenet100 -t simclr -b 512 -e 100 -o lars --lr 0.3 -w 1e-6 -u 10
+python train_self_supervised_task.py -d imagenet100 -t simclr -b 512 -e 300 -o lars --lr 0.3 -w 1e-6 -u 10
